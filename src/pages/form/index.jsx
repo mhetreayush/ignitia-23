03b17ba54bd05a7e11bbeab4c056ecd0a4f96d6d
Sharvil <93196443+Sharvil-Karwa@users.noreply.github.com>
2023-06-02 22:57:06 +0530

    Update index.jsx

--- src/pages/form/index.jsx
+++ src/pages/form/index.jsx
@@ -1,4 +1,33 @@
-const addCount = async;
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+  // Add your Firebase configuration here
+  // apiKey: "YOUR_API_KEY",
+  // authDomain: "YOUR_AUTH_DOMAIN",
+  // databaseURL: "YOUR_DATABASE_URL",
+  // projectId: "YOUR_PROJECT_ID",
+  // storageBucket: "YOUR_STORAGE_BUCKET",
+  // messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  // appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+const addCount = async () => {
+  // Retrieve the current count from the database
+  const countSnapshot = await database.ref('count').once('value');
+  let currentCount = countSnapshot.val();
+
+  // Increment the count by 1
+  currentCount += 1;
+
+  // Update the count in the database
+  await database.ref('count').set(currentCount);
+};
+
 const FormTemplate = () => {
   return <button onClick={addCount}>add count</button>;
 };
