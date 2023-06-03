47c70cf41085843fa4337920145358fe6442cf78
Ayush <mhetreayush1719@gmail.com>
2023-06-04 00:10:54 +0530

    Added scalability, USP and Target Audience

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -6,11 +6,11 @@ const PageWrapper = ({ children }) => {
       <div className="w-full p-4">
         <h1 className="text-2xl font-bold">ResearchPilot</h1>
       </div>
-      <div className="grid grid-cols-12">
-        <div className="col-span-2">
+      <div className="md:grid grid-cols-12">
+        <div className="hidden md:block col-span-2">
           <Sidebar />
         </div>
-        <div className="col-span-10 p-10">{children}</div>
+        <div className="col-span-10 p-4 md:p-10">{children}</div>
       </div>
     </div>
   );
