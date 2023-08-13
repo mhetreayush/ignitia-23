75c6f125d0722db6929eb096f7c3cc72a1c01085
Ayush <mhetreayush1719@gmail.com>
2023-08-13 14:13:26 +0530

    💼💼 Add recruiter login
    - Added recruiter login

--- src/pages/myForms/index.jsx
+++ src/pages/myForms/index.jsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 
 const Index = () => {
   const [forms, setForms] = useState([]);
+  const [formsFetched, setFormsFetched] = useState(false);
   const fetchForms = async () => {
     const user = JSON.parse(localStorage.getItem("user")).uid;
     const formsRef = doc(db, "users", user);
@@ -14,6 +15,7 @@ const Index = () => {
       setForms(formsSnap.data().forms);
       console.log(formsSnap.data().forms);
     }
+    setFormsFetched(true);
   };
   useEffect(() => {
     fetchForms();
@@ -21,7 +23,7 @@ const Index = () => {
   return (
     <PageWrapper>
       <div className="flex flex-col gap-y-4 w-3/4">
-        {forms.map((form, idx) => {
+        {forms?.map((form, idx) => {
           return (
             <Link
               className="flex flex-col w-full gap-y-4 p-4 bg-[#1F1926] rounded-md mt-4"
@@ -32,6 +34,17 @@ const Index = () => {
             </Link>
           );
         })}
+        {!formsFetched && (
+          <div className="flex justify-center items-center h-96">
+            <h1 className="text-2xl font-semibold">Fetching forms...</h1>
+          </div>
+        )}
+
+        {formsFetched && !forms && (
+          <div className="flex justify-center items-center h-96">
+            <h1 className="text-2xl font-semibold">No forms found</h1>
+          </div>
+        )}
       </div>
     </PageWrapper>
   );
